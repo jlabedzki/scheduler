fe7e5d6e9f305a5fa81a025b4a2ff967f60853ac
Johnny <johnnylabedzki@hotmail.com>
2021-10-01 10:46:24 -0700

    Create custom hook 'useVisualMode' and associated tests. All passed.

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(mode, boolean) {
+    if (boolean) {
+      history.pop();
+    }
+    setMode(mode);
+    history.push(mode);
+  };
+
+  function back() {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1]);
+    }
+  }
+
+  return { mode, transition, back }
+};
