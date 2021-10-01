342ad404d09362a87cbe0df2a3c3d2b0dcb6d84f
Johnny <johnnylabedzki@hotmail.com>
2021-10-01 10:11:39 -0700

    Install react hooks testing library and peer dependencies

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 import "components/Application.scss";
 
 export default function Application() {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   })
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
@@ -18,13 +19,18 @@ export default function Application() {
 
 
   useEffect(() => {
-    const promise1 = axios.get('/api/days');
-    const promise2 = axios.get('/api/appointments');
-
-    Promise.all([promise1, promise2])
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
+    ])
       .then(all => {
-        console.log(all);
-        setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }))
+        setState(prev => ({
+          ...prev,
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data
+        }))
       });
   }, []);
 
@@ -52,9 +58,13 @@ export default function Application() {
         />      </section>
       <section className="schedule">
         {dailyAppointments.map(appointment => {
+          const interview = getInterview(state, appointment.interview);
           return (
             <Appointment
-              key={appointment.id} {...appointment}
+              key={appointment.id}
+              id={appointment.id}
+              time={appointment.time}
+              interview={interview}
             />
           )
         })}
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -16,3 +16,18 @@ export function getAppointmentsForDay(state, day) {
 
   return filteredDays;
 };
+
+export function getInterview(state, interview) {
+  const interviewers = Object.keys(state.interviewers);
+
+  if (!interview) {
+    return null;
+  }
+
+  if (interviewers.includes(`${interview.interviewer}`)) {
+    return {
+      student: interview.student,
+      interviewer: state.interviewers[interview.interviewer]
+    };
+  }
+}
