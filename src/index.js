e5b13ac27656c6b4b7f9dbf0be25d707b014820e
Johnny <johnnylabedzki@hotmail.com>
2021-10-06 10:49:53 -0700

    Refactor based on netlify errors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "index.scss";
+import Application from "components/Application";
 import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
@@ -5,10 +7,5 @@ import axios from "axios";
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 }
-
-import "index.scss";
-
-import Application from "components/Application";
-
 
 ReactDOM.render(<Application />, document.getElementById("root"));
